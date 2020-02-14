119d6aa2490c796ba3c266bf08f058dae6ff9f97
tandt <v.tandt1@vinid.net>
2020-02-14 21:54:04 +0700

    first commit

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by TANDT1 on 2/14/20.
+//  Copyright © 2020 tandt53. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
